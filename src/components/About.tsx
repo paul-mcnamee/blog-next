111656ca667ed5a8a82f2aa082cc3167cc47cbb0
Paul McNamee <paulmc121@gmail.com>
2024-05-04 13:38:54 -0600

    style: adjust padding around about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ const About = () => {
               alt='Profile'
             />
           </div>
-          <div className='max-w-lg pr-5'>
+          <div className='max-w-lg pr-8'>
             <p className='mt-2 indent-4'>
               I create websites for small businesses and entrepreneurs to
               establish an online presence and flourish in the digital age. I
